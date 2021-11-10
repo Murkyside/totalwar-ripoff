f6beed749fd4a1957fc94997af4c27eb63a3dd79
Murkyside <49945541+Murkyside@users.noreply.github.com>
2021-11-10 15:34:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+class Unit():
+
+  def __init__(self,kind,number,x,y,theta):
+    self.type = kind # what type of soldier
+    self.number = number # how many men are still in the unit
+    self.x = x
+    self.y = y
+    self.theta = theta #this is the rotation of the unit
+    
+  def rotate(self,direction):
+
+    return()
+
+
+  def translate(self, i, j):
+
+    return()
+    
+
+#oi kaylem: a1, a2, b1, b2 are the corners of the rectangle
+#https://stackoverflow.com/questions/306316/determine-if-two-rectangles-overlap-each-other
+
+
+
+
+def main():
+  print('hello')
