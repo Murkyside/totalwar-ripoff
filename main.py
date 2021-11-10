41723cf58a80322aedf758b2697969ce7dc658c4
Murkyside <49945541+Murkyside@users.noreply.github.com>
2021-11-10 20:46:23 +0000

    Finished Rotation

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
 import math
+from guizero import App, Drawing 
+
+w = 600
+h = 600
+
+app = App(width=w, height=h)  
+drawing = Drawing(app, width=w, height=h)
+
+def trans(x):
+  return((x+20)*5)
+
+
 
 class Unit():
 
   def __init__(self,kind,number,x,y,theta):
     self.type = kind # what type of soldier
     self.number = number # how many men are still in the unit
-    self.x = x
-    self.y = y
     self.theta = theta #this is the rotation of the unit
-    
-  def rotate(self,direction):
-    # rotate soldiers by 30 degrees, amount depends on input
-    # special numbers - 63.435 & 116.565: DO NOT CHANGE
-    a1 = [5**(1/2)*math.sin(self.theta-63.435), 5**(1/2)*math.cos(self.theta-63.435)]
-    a2 = [5**(1/2)*math.sin(self.theta+63.435), 5**(1/2)*math.cos(self.theta+63.435)]
-    b1 = [5**(1/2)*math.sin(self.theta+116.565), 5**(1/2)*math.cos(self.theta+116.565)]
-    b2 = [5**(1/2)*math.sin(self.theta+243.435), 5**(1/2)*math.cos(self.theta+243.435)]
-    print(a1,a2,b1,b2)
-    
 
+  def updateRot(self):
+    self.a1 = [5**(1/2)*math.cos(self.theta-1.107), 5**(1/2)*math.sin(self.theta-1.107)]
+    self.a2 = [5**(1/2)*math.cos(self.theta+1.107), 5**(1/2)*math.sin(self.theta+1.107)]
+    self.b1 = [5**(1/2)*math.cos(self.theta+2.0344), 5**(1/2)*math.sin(self.theta+2.0344)]
+    self.b2 = [5**(1/2)*math.cos(self.theta+4.2874), 5**(1/2)*math.sin(self.theta+4.2874)]
 
+  def rotateCW(self):
+    self.theta = self.theta + math.pi/6
+    self.updateRot()
+    
+  def rotateCCW(self):
+    self.theta = self.theta - math.pi/6
+    self.updateRot()
+
+  def drawUnit(self):
+    drawing.rectangle(trans(self.a1[0]),trans(self.a1[1]),trans(self.a1[0])+5,trans(self.a1[1])+5,color = 'red')
+    drawing.rectangle(trans(self.a2[0]),trans(self.a2[1]),trans(self.a2[0])+5,trans(self.a2[1])+5,color = 'red')
+    drawing.rectangle(trans(self.b1[0]),trans(self.b1[1]),trans(self.b1[0])+5,trans(self.b1[1])+5,color = 'red')
+    drawing.rectangle(trans(self.b2[0]),trans(self.b2[1]),trans(self.b2[0])+5,trans(self.b2[1])+5,color = 'red')
+    
   def translate(self, i, j):
-
-    return()
+    self.a1[0] = self.a1[0]+i
+    self.a2[1] = self.a2[1]+j
+    self.b1[0] = self.b1[0]+i
+    self.b2[1] = self.b2[1]+j
 
 test = Unit('Infantry',1000,10,10,0)
-test.rotate(0)
+test.rotateCW()
 
 #oi kaylem: a1, a2, b1, b2 are the corners of the rectangle
 #https://stackoverflow.com/questions/306316/determine-if-two-rectangles-overlap-each-other
